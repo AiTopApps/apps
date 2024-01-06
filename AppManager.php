accf59f615980167dbd5d30c78d49305723e80d2
AiTopApps <153239948+AiTopApps@users.noreply.github.com>
2024-01-06 14:23:30 +0100

    Update AppManager.php

--- AppManager.php
+++ AppManager.php
@@ -3,9 +3,9 @@
      
             "PopupEnabled":"true",
             
-           "App1":"Monster Hunter",
-           "Link1":"com.netmarble.kofg",
-           "Icon1":"https://play-lh.googleusercontent.com/JJ5zXv997qZ0UNzq5TcetDU1cWRxmCisFIeYNlEpyBE2X9TQzVGRTDhqym-YhaGVL9bl",
+           "App1":"Cute Girly Wallpaper",
+           "Link1":"com.girlywallpaper.wallpapers.cutewallpaper.offlinewallpaper.butterfly.unicorns.girls",
+           "Icon1":"https://play-lh.googleusercontent.com/Epf-lqkIfQvR_UK7Wtbd6OsAk6YaJ971jh7QBL53wOipSIJ-T4je83a_BqQyCEiPL0U=w240-h480-rw",
             
            "App2":"Street Fighter",
            "Link2":"com.netmarble.kofg",
